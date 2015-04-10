7f7d891f35427832a36f173e37486c6679f8adca
Marin Tomic <tomic.marin@gmail.com>
2015-04-11 00:57:24 +0200

    Updated navigation.

--- generate.py
+++ generate.py
@@ -4,29 +4,23 @@ from spreadsheet_parser.data_manager import DataManager
 from jinja2 import Environment, FileSystemLoader
 
 manager = DataManager()
-contests = manager.get_contests()
+tasks = manager.get_tasks()
 
-contest_groups = sorted(set([
-    (contest.short_name, contest.full_name) for contest in contests
-]))
+task_tree = {}
 
-# generiranje HTML-a
+unique_id = 0
+
+for task in tasks:
+    levels = task.contests_key.split('-')
+    node = task_tree
+    for level in levels:
+        node = node.setdefault(level, (unique_id, {}))[1]
+        unique_id = unique_id+1
+    node.setdefault('-tasks', []).append(task)
+
 env = Environment(loader=FileSystemLoader('templates'))
-
-contest_list_template = env.get_template('contest_list.html')
 task_list_template = env.get_template('task_list.html')
 
 f = open('public_html/tasks.html', 'w')
-f.write(contest_list_template.render(contests=contest_groups).encode('utf-8'))
+f.write(task_list_template.render(task_tree=task_tree).encode('utf8'))
 f.close()
-
-for (contest_group_name, contest_group_full_name) in contest_groups:
-    contest_group = filter(lambda ct: ct.short_name == contest_group_name, contests)
-
-    f = open('public_html/contests/%s.html' % contest_group_name, 'w')
-    f.write(task_list_template.render(
-                name=contest_group_full_name, 
-                contest_list=contest_group, 
-                manager=manager
-                ).encode('utf-8'))
-    f.close()
