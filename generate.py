def84d7c7995a323a68beb025557c52c0c550987
Marin Tomic <tomic.marin@gmail.com>
2015-04-11 13:53:08 +0200

    New design.

--- generate.py
+++ generate.py
@@ -2,25 +2,32 @@
 
 from spreadsheet_parser.data_manager import DataManager
 from jinja2 import Environment, FileSystemLoader
+import itertools
 
 manager = DataManager()
-tasks = manager.get_tasks()
+contests = manager.get_contests()
 
-task_tree = {}
+contest_tree = {}
 
-unique_id = 0
-
-for task in tasks:
-    levels = task.contests_key.split('-')
-    node = task_tree
-    for level in levels:
-        node = node.setdefault(level, (unique_id, {}))[1]
-        unique_id = unique_id+1
-    node.setdefault('-tasks', []).append(task)
+for contest in contests:
+    ct = contest_tree.setdefault(contest.short_name, {})
+    ct.setdefault(contest.year, []).append(contest)
 
 env = Environment(loader=FileSystemLoader('templates'))
-task_list_template = env.get_template('task_list.html')
+contest_list_template = env.get_template('contest_list.html')
+contest_template = env.get_template('contest.html')
 
-f = open('public_html/tasks.html', 'w')
-f.write(task_list_template.render(task_tree=task_tree).encode('utf8'))
+f = open('public_html/contests.html', 'w')
+f.write(contest_list_template.render(contest_tree=contest_tree).encode('utf8'))
 f.close()
+
+for name, years in contest_tree.iteritems():
+    for year, rounds in years.iteritems():
+        f = open('public_html/contests/%s_%s.html' % (name, year), 'w')
+        f.write(contest_template.render(
+            manager=manager,
+            name=name,
+            year=year,
+            rounds=rounds
+        ).encode('utf8'))
+        f.close()
