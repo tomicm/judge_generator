f2701ddecbcb7211a70b4977200552e8e1390cfd
Marin Tomic <tomic.marin@gmail.com>
2015-04-22 21:27:24 +0200

    minor changes

---
+++ templates/js/search_client.js
@@ -0,0 +1,18 @@
+function SearchCtrl($scope) {
+  
+  $scope.tasks = [];
+
+  $scope.updateFound = function() {
+    $scope.tasks = Search.search($scope.keywords).map(function(result) { 
+      var task_id = 'tasks/' + result.split(' ')[0];
+      var task_info = get_task_info(task_id);
+      var pdf_path = get_task_pdf_path(task_id);
+      return [
+        task_id,
+        task_info.task_name,
+        task_info.contest_name 
+         + (task_info.round_name?', '+task_info.round_name.toLowerCase() : ''),
+        get_task_pdf_path(task_id)];
+    });
+  }
+}
